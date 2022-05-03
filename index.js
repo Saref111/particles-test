86db127d5acc8e6a49e6dcd9dcbab881ab8f206e
Saref111 <saref012@gmail.com>
2022-05-03 14:35:37 +0300

    created particles

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+import Particle from "./particle.js";
+
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
 
+let particles = [];
+
+const animate = (timer) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(particle => {
+        particle.update();
+        particle.draw(ctx);
+        console.log(particle);
+    });
+    requestAnimationFrame(animate);
+}
+
+const init = () => {
+    for (let i = 0; i < 50; i++) {
+        particles.push(new Particle(
+            Math.random() * canvas.width,
+            Math.random() * canvas.height, 
+            Math.random() * 10, 
+            '#000'
+        ));
+    }
+    animate()
+}
+
+
+init();
---
+++ particle.js
@@ -0,0 +1,24 @@
+export default class Particle {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = {
+            x: (Math.random() - 0.5) * 10,
+            y: (Math.random() - 0.5) * 10
+        };
+    }
+
+    update() {
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
