abd8678573dc6839de9b622682eb4b8d237ff831
Saref111 <saref012@gmail.com>
2022-05-03 14:26:26 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const canvas = document.createElement('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+document.body.appendChild(canvas);
+
