fdfc7f71bf9952c96c7b7f15d5277dfc93c0a3e8
Saref111 <saref012@gmail.com>
2022-05-03 15:17:02 +0300

    added connection

--- index.js
+++ index.js
@@ -6,7 +6,42 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
 
+const MOUSE_GRAVITY = 0.1;
+const CONNECT_DISTANCE = (canvas.width / 90) * (canvas.height / 90);
+
+let deltaTime = 0;
 let particles = [];
+let mouse = {
+    x: undefined,
+    y: undefined,
+    radius: (canvas.width / 80) * (canvas.width / 80),
+}
+
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+})
+
+window.addEventListener('touchstart', (e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+})
+
+const connect = (particle, i) => {
+    for (let j = i; j < particles.length; j++) {
+        const dx = particle.x - particles[j].x;
+        const dy = particle.y - particles[j].y;
+
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < CONNECT_DISTANCE) {
+            ctx.beginPath();
+            ctx.moveTo(particle.x, particle.y);
+            ctx.lineTo(particles[j].x, particles[j].y);
+            ctx.stroke();
+        }
+    }
+}
 
 const changeVelocity = (particle) => {
     if (particle.x > canvas.width || particle.x < 0) {
@@ -17,18 +52,44 @@ const changeVelocity = (particle) => {
     }
 }
 
-const animate = () => {
+const reactOnMouse = (particle) => {
+    let dx = mouse.x - particle.x;
+    let dy = mouse.y - particle.y;
+
+    if (Math.sqrt(dx * dx + dy * dy) < mouse.radius + particle.radius) {
+        if (particle.x > mouse.x) {
+            particle.velocity.x += MOUSE_GRAVITY;
+        }
+        if (particle.x < mouse.x) {
+            particle.velocity.x -= MOUSE_GRAVITY;
+        }
+        if (particle.y > mouse.y) {
+            particle.velocity.y += MOUSE_GRAVITY;
+        }
+        if (particle.y < mouse.y) {
+            particle.velocity.y -= MOUSE_GRAVITY;
+        }
+    }
+}
+
+const animate = (timestamp) => {
+    console.log(timestamp);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particles.forEach((particle) => {
+    particles.forEach((particle, i) => {
         particle.update();
         changeVelocity(particle);
+
+        reactOnMouse(particle);
+
+        connect(particle, i);
+
         particle.draw(ctx);
     });
     requestAnimationFrame(animate);
 }
 
 const init = () => {
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 100; i++) {
         particles.push(new Particle(
             Math.random() * canvas.width,
             Math.random() * canvas.height, 
@@ -36,7 +97,7 @@ const init = () => {
             '#000'
         ));
     }
-    animate()
+    animate();
 }
 
 
